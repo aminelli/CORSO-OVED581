3b0d8479bda6ab523e23b0dba4e635ff2f0e114e
aminelli <56305052+aminelli@users.noreply.github.com>
2024-06-04 10:49:41 +0200

    dfas

--- 00-Start/commands.sh
+++ 00-Start/commands.sh
@@ -4,3 +4,9 @@ sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noa
 # Installazione webconsole cokcpit
 sudo yum install cockpit
 sudo systemctl enable --now cockpit.socket
+
+# Comandi per visualizzare i servizi
+sudo systemctl status
+
+# Lista di tutti i servizi attivi
+sudo systemctl list-units --type=service --state=active
