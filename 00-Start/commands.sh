d74263879cdb1eb59afad7a0ab8430067cfbdda2
aminelli <56305052+aminelli@users.noreply.github.com>
2024-06-04 10:32:11 +0200

    ee

--- 00-Start/commands.sh
+++ 00-Start/commands.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+
+# Installazione webconsole cokcpit
+sudo yum install cockpit
+sudo systemctl enable --now cockpit.socket
